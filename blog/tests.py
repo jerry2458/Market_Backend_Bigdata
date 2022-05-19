a3c52792c9abae134ccc380e6c29202d3f9a2896
Hongin Lim <96184680+Hongin-Lim@users.noreply.github.com>
2022-05-19 16:34:08 +0900

    네비게이션 바와 푸터 모듈

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
 # Create your tests here.
+
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+
+        reponse = self.client.get('/blog/')
+        self.assertEqual(reponse.status_code, 200)
+        soup = BeautifulSoup(reponse.content, 'html.parser')
+        self.assertEqual(soup.title.text, 'Blog')
+
