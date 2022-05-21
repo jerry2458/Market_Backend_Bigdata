683571e99383dec5f928fd9efae54bbf8e904f30
Hongin Lim <96184680+Hongin-Lim@users.noreply.github.com>
2022-05-21 22:17:34 +0900

    포스트 목록 페이지에 카테고리 추가

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,17 @@ from django.db import models
 import os
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -13,7 +24,8 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    categoty = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 # Create your views here.
-from blog.models import Post
+from blog.models import Post, Category
 from django.views.generic import ListView, DetailView
 
 
 class Postlist(ListView):
     model = Post
     ordering = '-pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(Postlist, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count
+        return context
 
 class Postdetail(DetailView):
     model = Post
