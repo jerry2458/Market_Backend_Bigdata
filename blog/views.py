d6f3c130c54e084008d9e51643dca93c54113761
Hongin Lim <96184680+Hongin-Lim@users.noreply.github.com>
2022-05-22 21:32:13 +0900

    댓글 수정구현

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
 from django.utils.text import slugify
 from django.shortcuts import get_object_or_404
-from blog.models import Post, Category, Tag
+from blog.models import Post, Category, Tag, Comment
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .forms import CommentForm
@@ -155,5 +155,16 @@ def new_comment(request, pk):
                 return redirect(comment.get_absolute_url())
             else:
                 return redirect(post.get_absolute_url())
+        else:
+            raise PermissionDenied
+
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
