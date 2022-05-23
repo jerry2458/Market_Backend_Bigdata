e893febe404ec9f2bd70e0dbfc914ef965c2cdc7
Hongin Lim <96184680+Hongin-Lim@users.noreply.github.com>
2022-05-24 01:57:01 +0900

    검색 기능 구현

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from blog.models import Post, Category, Tag, Comment
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .forms import CommentForm
+from django.db.models import Q
 
 
 class Postlist(ListView):
@@ -177,3 +178,20 @@ def delete_comment(request, pk):
         return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
+
+class PostSearch(Postlist):
+    paginate_by = None
+
+    def get_queryset(self):
+        q = self.kwargs['q']
+        post_list = Post.objects.filter(
+            Q(title__contains=q) | Q(tags__name__contains=q)
+        ).distinct()
+        return post_list
+
+    def get_context_data(self, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+
+        return context
